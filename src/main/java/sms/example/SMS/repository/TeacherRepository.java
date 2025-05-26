985d7d489a1652b6eaaa8338e8139607fbc4348b
mfique <pacifiquem58@gmail.com>
2025-05-26 11:42:54 +0200

    changes

--- src/main/java/sms/example/SMS/repository/TeacherRepository.java
+++ src/main/java/sms/example/SMS/repository/TeacherRepository.java
@@ -4,4 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import sms.example.SMS.entity.Teacher;
 
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
-} 
+}
